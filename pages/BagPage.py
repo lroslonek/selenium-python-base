0bb5eb463856c16e3bd9a7bfcb0050be8f4e2134
Lukasz Roslonek <lukasz.roslonek@allegro.pl>
2018-11-26 16:43:28 +0100

    full test - to refactor

--- pages/BagPage.py
+++ pages/BagPage.py
@@ -1,15 +1,16 @@
 import logging
-
+from decimal import Decimal
 from selenium.webdriver.common.by import By
-
 from pages.BasePage import BasePage
-from utils.WebdriverWaits import WebdriverWaits
 
 
 class BagPage(BasePage):
 
     __increase_quantity_locator = (By.XPATH, '//*[@id="dnn_ctr1628848_ViewBasket_BasketDetails_gvBasketDetails"]/table/tbody/tr/td[4]/div/a[2]')
+    __item_price_locator = (By.XPATH, '//*[@id="dnn_ctr1628848_ViewBasket_BasketDetails_gvBasketDetails"]/table/tbody/tr/td[5]/span[2]')
+    __total_price_locator = (By.XPATH, '//*[@id="dnn_ctr1628848_ViewBasket_BasketDetails_gvBasketDetails"]/table/tbody/tr/td[6]/span[2]')
     __update_bag_locator = (By.CLASS_NAME, 'NewUpdateQuant')
+
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -21,3 +22,18 @@ class BagPage(BasePage):
     def update_bag(self):
         self.driver.find_element(*self.__update_bag_locator).click()
 
+    def is_total_price_correct(self):
+        item_price = self.__get_decimal_value(self.__item_price_locator)
+        total_price = self.__get_decimal_value(self.__total_price_locator)
+        logging.info("item price is: {}".format(str(item_price)))
+        logging.info("price for two items is: {}".format(str(total_price)))
+
+        if 2 * item_price == total_price:
+            logging.info("prices are correct!")
+            return True
+        else:
+            logging.error("prices are NOT correct! :(")
+            return False
+
+    def __get_decimal_value(self, locator):
+        return Decimal(self.driver.find_element(*locator).text.strip("£"))
--- tests/SportsDirectCheckoutTest.py
+++ tests/SportsDirectCheckoutTest.py
@@ -11,7 +11,7 @@ class GoogleQueryTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome('../driver/chromedriver')
 
-    def test_google_query(self):
+    def test_sports_direct_bag_prices(self):
         page = MainPage(self.driver)
         page.dismiss_modals()
         page = page.search_for_phrase("Garmin forerunner")
@@ -20,9 +20,8 @@ class GoogleQueryTest(unittest.TestCase):
         page = page.view_bag()
         page.increase_product_quantity()
         page.update_bag()
-
         sleep(3)
-        assert True
+        assert page.is_total_price_correct()
 
     def tearDown(self):
         self.driver.quit()
