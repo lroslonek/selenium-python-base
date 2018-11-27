a22386d1c9437ac3e6d55cf4dc4b150e6922502c
Lukasz Roslonek <lukasz.roslonek@allegro.pl>
2018-11-27 15:10:37 +0100

    after fixes

--- tests/test_SportsDirect.py
+++ tests/test_SportsDirect.py
@@ -1,11 +1,10 @@
-import unittest
-
+import logging
+from unittest import TestCase
 from selenium import webdriver
-
 from pages.MainPage import MainPage
 
 
-class PricesInBagTest(unittest.TestCase):
+class PricesInBagTest(TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome('../driver/chromedriver')
@@ -14,6 +13,7 @@ class PricesInBagTest(unittest.TestCase):
         self.driver.quit()
 
     def test_should_item_and_total_bag_prices_be_correct(self):
+        logging.info("")
         # given
         main_page = MainPage(self.driver)
         main_page.dismiss_modals()
@@ -30,3 +30,15 @@ class PricesInBagTest(unittest.TestCase):
 
         # then
         assert bag_page.is_total_price_correct()
+
+    def test_should_not_sign_in_unregistered_user(self):
+        # given
+        main_page = MainPage(self.driver)
+        main_page.dismiss_modals()
+
+        # when
+        login_page = main_page.go_to_sign_in_page()
+        login_page.sign_in("unregistered@foo.com", "unregistered")
+
+        # then
+        assert login_page.is_not_signed_in("This email address or password is incorrect")
